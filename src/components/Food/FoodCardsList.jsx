edb54c73598d1fd85582ea9f01a1c7f14892e00a
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-03-01 23:14:35 +0100

    working on fetch available food

--- src/components/Food/FoodCardsList.jsx
+++ src/components/Food/FoodCardsList.jsx
@@ -1,6 +1,35 @@
 import FoodCard from "./FoodCard";
+import { fetchAvailableFood } from "../../http";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function FoodCardsList() {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState();
+
+  async function getAvailableFood() {
+    try {
+      await fetchAvailableFood();
+    } catch (error) {
+      setError({
+        message: error.message || "Failed to fetch user places",
+      });
+    }
+  }
+
+  useEffect(() => {
+    async function getAvailableFood() {
+      try {
+        await fetchAvailableFood();
+      } catch (error) {
+        setError({
+          message: error.message || "Failed to fetch available food",
+        });
+      }
+    }
+  }, []);
+
   return (
     <ul id="food-cards-list">
       {/* here .map */}
