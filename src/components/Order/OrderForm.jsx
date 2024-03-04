dbc32e2b025a85aaa0772f072ae39da8ff9fac88
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-03-04 02:18:19 +0100

    working on user details on form

--- src/components/Order/OrderForm.jsx
+++ src/components/Order/OrderForm.jsx
@@ -1,9 +1,14 @@
+import { useRef, useState } from "react";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
 
 export default function OrderForm({ onClose, totPrice, onForm }) {
+  const [userDetails, setUserDetails] = useState({});
+
+  function getUserDetails() {}
+
   return (
-    <div id="order-form">
+    <form id="order-form">
       <Button
         onClick={() => {
           onForm(false);
@@ -13,13 +18,18 @@ export default function OrderForm({ onClose, totPrice, onForm }) {
       />
       <h1>Checkout</h1>
       <div>Total Amount: ${totPrice}</div>
-      <Input label="Full Name" />
-      <Input label="E-Email Address" type="email" />
-      <Input label="Street" />
+      <Input label="Full Name" name="fullName" />
+      <Input label="E-Email Address" type="email" name="email" />
+      <Input label="Street" name="street" />
 
       <div>
-        <Input inpStyle="user-detail short" label="Postal Code" type="num" />
-        <Input inpStyle="user-detail short" label="City" />
+        <Input
+          inpStyle="user-detail short"
+          label="Postal Code"
+          type="num"
+          name="postalCode"
+        />
+        <Input inpStyle="user-detail short" label="City" name="city" />
       </div>
 
       <div className="cart-buttons">
@@ -30,8 +40,8 @@ export default function OrderForm({ onClose, totPrice, onForm }) {
             onClose();
           }}
         />
-        <Button text="Submit Order" btnStyle="btn-bg" />
+        <Button text="Submit Order" btnStyle="btn-bg" type="submit" />
       </div>
-    </div>
+    </form>
   );
 }
